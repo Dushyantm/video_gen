586ad91df13f1a4f6036cadb096efbb1acb82e78
Dushyantm <dummahajan@gmail.com>
2022-02-14 22:11:45 +0530

    aws

--- aws_image.py
+++ aws_image.py
@@ -1,3 +1,14 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def import_image(props):
+    
+    s3 = boto3.client("s3", 
+                  region_name='ap-southeast-1', 
+                  aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+                  aws_secret_access_key=os.getenv("AWS_SECRET_KEY"))
     list = props[0]
     print(list['img_back'])
