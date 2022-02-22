bfb55b5c6899ce94e5d59ba4d2c6d4a380ade2bf
Dushyantm <dummahajan@gmail.com>
2022-02-22 16:51:50 +0530

    frames built

--- image.py
+++ image.py
@@ -1,26 +1,30 @@
-from tkinter import font
+
 from PIL import Image, ImageDraw, ImageFont
-import textwrap
 
-def buildImage():
-    image = Image.new("RGB", (1920,1080), (255, 255, 255))
-    frnt1 = Image.open(r"images\img0.jpg") 
-    back1 = Image.open(r"images\img1.jpg")
-    name1 = Image.open(r"images\img2.jpg")
-    newsize = (350,350)
-    name_size = (250,100)
-    frnt1= frnt1.resize(newsize)
-    back1= back1.resize(newsize)
-    name1 = name1.resize(name_size)
-    d1 = ImageDraw.Draw(image)
+
+def buildImage(list, images ,text):
     myFont = ImageFont.truetype('font/Roboto-Regular.ttf', 65)
-    myFont1 = ImageFont.truetype('font/Roboto-Bold.ttf', 65) 
-    d1.text((700,120),"Norcin 400",font=myFont1,fill=(255,0,0))
-    d1.text((950, 230), "Two tablets should be taken \nin the morning and evening \nafter your meal. \nThis tablet should be taken for \nten days.", font = myFont,  fill =(0, 0, 0))
-    Image.Image.paste(image,frnt1,(120,250))
-    Image.Image.paste(image,back1,(470,250))
-    Image.Image.paste(image,name1,(300,610))
-    image.save('img1.png')
-    image.show()
-
-buildImage()
+    myFont1 = ImageFont.truetype('font/Roboto-Bold.ttf', 65)
+    
+    frames = []
+    for i in range (len(list)):
+        drug_name = str(list[i]['drug_name'])
+        text_str = str(text[i])
+        bg_image = Image.new("RGB", (1920,1080), (255, 255, 255))
+        frnt1 = images[i][0]
+        back1 = images[i][1]
+        name1 = images[i][2]
+        newsize = (350,350)
+        name_size = (250,100)
+        frnt1= frnt1.resize(newsize)
+        back1= back1.resize(newsize)
+        name1 = name1.resize(name_size)
+        f1 = ImageDraw.Draw(bg_image)        
+        f1.text((700,120), drug_name, font=myFont1,fill=(255,0,0))
+        f1.text((950, 230), text_str, font = myFont,  fill =(0, 0, 0))
+        Image.Image.paste(bg_image,frnt1,(120,250))
+        Image.Image.paste(bg_image,back1,(470,250))
+        Image.Image.paste(bg_image,name1,(300,610))
+        bg_image.show()
+        frames.append(bg_image)
+    print(frames)
--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from PIL import Image, ImageDraw
 from text_script import text_script
 from aws_image import import_image
 from image import buildImage
-list = []
 
 
 input = [{'id': '1644402640913', 'data': {'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Ceemox-250', 'type': 'Capsule', 'time': ['Morning'], 'quantity': '5', 'ba': 'before', 'days': '15', 'img_front': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Front.jpg', 'img_back': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Back.jpg', 'img_name': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Name.jpg', 'spcl_intr': ''}}, {'id': '1644402655353', 'data': {'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Norcin-400', 'type': 'Tablet', 'time': ['Morning', 'Evening'], 'quantity': '2', 'ba': 'after', 'days': '10', 'img_front': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Front.jpg', 'img_back': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Back.jpg', 'img_name': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Name.jpg', 'spcl_intr': ''}}]
-for i in range (len(input)):
-    dict = input[i] 
-    data = dict["data"]
-    list.append(data)
+
+def build_video(input):
+    list = []
+    for i in range (len(input)):
+        dict = input[i] 
+        data = dict["data"]
+        list.append(data)
     
-aws_image = import_image(list)
-buildImage(aws_image)
-# text_script(list)
+    #fetch drug images
+    aws_image = import_image(list)
+
+    #generate text
+    text = text_script(list)
+
+    #use drug images and text to build frames
+    buildImage(list, aws_image,text)
+
+build_video(input)
