e6d7432d8c8550c371592017cdf258724aec3efe
Dushyantm <dummahajan@gmail.com>
2022-02-28 14:16:00 +0530

    audio and text modules

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PIL import Image, ImageDraw
-from text_script import text_script
+from audio import audio_clip
+from text_script import getSentence
 from aws_image import import_image
-from image import buildImage
+from buildImage import buildImages
+from video import videoGen
 
 
 input = [{'id': '1644402640913', 'data': {'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Ceemox-250', 'type': 'Capsule', 'time': ['Morning'], 'quantity': '5',  'language': 'Marathi','ba': 'before', 'days': '15', 'img_front': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Front.jpg', 'img_back': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Back.jpg', 'img_name': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Name.jpg', 'spcl_intr': ''}}, {'id': '1644402655353', 'data': {'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Norcin-400', 'type': 'Tablet', 'time': ['Morning', 'Evening'], 'quantity': '2', 'language': 'English', 'ba': 'after', 'days': '10', 'img_front': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Front.jpg', 'img_back': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Back.jpg', 'img_name': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Name.jpg', 'spcl_intr': ''}}]
@@ -16,10 +18,15 @@ def build_video(input):
     #fetch drug images
     aws_image = import_image(list)
 
+    #generate audio
+    audio_list = audio_clip
+
     #generate text
-    text = text_script(list)
+    text = getSentence(list)
 
     #use drug images and text to build frames
-    buildImage(list, aws_image,text)
+    frames = buildImages(list, aws_image,text)
 
+    final_video = videoGen(frames,audio_list)
+    
 build_video(input)
