c9e6e95f9edfeb8e34a5a1c9aeb06137246c5018
Suryakant <suryakantk94@gmail.com>
2022-03-09 14:50:46 +0530

    Changes fixed"

--- video.py
+++ video.py
@@ -3,7 +3,7 @@ import text_script
 from audio import audio_clip
 from PIL import Image
 import numpy as np
-
+import glob
 
 #this function changes the PIL image to numpy array 
 def image_to_array(frames):
@@ -16,9 +16,8 @@ def video_generator(frames,audio_clips):
 
     video_clips = []
 
-    #frames = [Image.open(i) for i in frames]
     frames_array = image_to_array(frames)
-
+    
     mp = ImageClip(frames_array[0])
     mp = mp.set_duration(3)
     video_clips.append(mp)
@@ -41,7 +40,7 @@ def video_generator(frames,audio_clips):
 
         video_clips.append(clip)
 
-    video_clips.append(intr2)
+    video_clips.append(intr2) 
     #concatenate the video clips
     final = concatenate_videoclips(video_clips,method='compose')
     final.write_videofile('final.mp4',fps=1)
@@ -52,10 +51,16 @@ def video_generator(frames,audio_clips):
 #the below code is for module tests only
 input = [{'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Ceemox-250', 'type': 'Capsule', 'time': ['Morning'], 'quantity': '5', 'language': 'Marathi', 'ba': 'before', 'days': '15', 'img_front': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Front.jpg', 'img_back': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Back.jpg', 'img_name': 'MP_medicineDB/Capsule/Ceemox-250_Anrose Pharma_Saralife Healthcare_Name.jpg', 'spcl_intr': ''}, {'patientId': '60efb3be62512a00158190ca', 'drug_name': 'Norcin-400', 'type': 'Tablet', 'time': ['Morning', 'Afternoon'], 'quantity': '2', 'language': 'English', 'ba': 'after', 'days': '10', 'img_front': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Front.jpg', 'img_back': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Back.jpg', 'img_name': 'MP_medicineDB/Tablet/Norcin-400_Omega Biotech_Name.jpg', 'spcl_intr': ''}]
 
-frames = ['MP.png','intr1.png','img1.png','img2.png','intr2.png']
+# frames = ['MP.png','intr1.png','img1.png','img2.png','intr2.png']
+
 sentences = text_script.getSentence(input)
 audio_clips = audio_clip(input,sentences)
 
-frames = [Image.open(i) for i in frames]
+frames = []
+for img in glob.glob("framesFolder/*.png"):
+    image = Image.open(img)
+    frames.append(image)
+  
 video_generator(frames,audio_clips)
+
 
