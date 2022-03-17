0f5eca994aa6509cb5a2581068ed66b3a82cc08d
Raj Aryan <rajaryankuntal@gmail.com>
2022-03-17 21:44:56 +0530

    API updated

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, json, request
 from flask_cors import CORS,cross_origin
 import sys
 import main
+from base64 import b64encode
 import json
 import numpy
 app = Flask(__name__)
@@ -18,5 +19,10 @@ def send_video():
 
     input = request.get_json()['data']
     video = main.build_video(input)
-    #file = open('final.mp4','rb').read()
-    return jsonify({'video_data' : "file"})
+    file = open('final.mp4','rb')
+    file.seek(0)
+    data = b64encode(file.read())
+    base64_string = data.decode('utf-8')
+    raw_data = {'video':base64_string}
+    data = json.dumps(raw_data)
+    return data
