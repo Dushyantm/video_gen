71ac2e51e9ed52c8c9437e99efb74522525558a2
Raj Aryan <rajaryankuntal@gmail.com>
2022-03-17 12:30:15 +0530

    requirements complete

--- flaskapp.py
+++ flaskapp.py
@@ -20,6 +20,3 @@ def send_video():
     video = numpy.array(main.build_video(input))
     file = {"video":"video generated"}
     return jsonify(file)
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
