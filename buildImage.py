3627ad7bd047575139c25123e0ef0e16d76435a2
Raj Aryan <rajaryankuntal@gmail.com>
2022-03-23 13:39:03 +0530

    bug fix

--- buildImage.py
+++ buildImage.py
@@ -3,7 +3,8 @@ from PIL import Image, ImageDraw, ImageFont
 def buildImages(list, images ,text):
     myFont = ImageFont.truetype('font/Roboto-Regular.ttf', 65)
     myFont1 = ImageFont.truetype('font/Roboto-Bold.ttf', 65)
-    MaHiFont = ImageFont.truetype('font/Nirmala.ttf', 65)
+    MarathiFont = ImageFont.truetype('font/Nirmala.ttf', 65)
+    HindiFont = ImageFont.truetype('font/Mukta-Light.ttf',65)
     frames = []
 
     for i in range (len(list)):
@@ -22,9 +23,9 @@ def buildImages(list, images ,text):
         f1.text((700,120), drug_name, font=myFont1,fill=(255,0,0))
         #condition to check language and use font accordingly.
         if(list[i]['language']=='Hindi'):
-            f1.text((950, 230), text_str, font=MaHiFont, fill=(0, 0, 0))
+            f1.text((950, 230), text_str, font=HindiFont, fill=(0, 0, 0))
         if(list[i]['language']=='Marathi'):
-            f1.text((950, 230), text_str, font=MaHiFont, fill=(0, 0, 0))
+            f1.text((950, 230), text_str, font=MarathiFont, fill=(0, 0, 0))
         else:
             f1.text((950, 230), text_str, font = myFont,  fill =(0, 0, 0))
         Image.Image.paste(bg_image, frnt1, (120,250))
